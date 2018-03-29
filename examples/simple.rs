2eee61ec6d9fba3dda7ccd44c5f42a7cd08f7e3b
Jacob Brown <kardeiz@gmail.com>
2018-03-29 12:13:27 -0500

    update to simpify

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,153 +1,28 @@
-extern crate futures;
-extern crate hyper;
-extern crate reset_router;
-extern crate num_cpus;
-
 extern crate http;
+extern crate reset_router;
+extern crate hyper;
 
-#[macro_use]
-extern crate error_chain;
+use std::sync::Arc;
 
-extern crate futures_fs;
-extern crate conduit_mime_types;
+type Response = ::http::Response<::hyper::Body>;
 
-extern crate tokio_core;
-extern crate net2;
 
-use reset_router::hyper::{Context, Router};
-
-use futures::Stream;
-use futures::Future;
-
-#[macro_use]
-extern crate lazy_static;
-
-use hyper::header::{ContentLength, ContentType};
-use hyper::Response;
-
-pub mod err {
-
-    error_chain! {
-        errors { }
-
-        foreign_links {
-            Io(::std::io::Error);
-            Hyper(::hyper::Error);
-        }
-    }
-
-    impl ::reset_router::hyper::IntoResponse for Error {
-        fn into_response(self) -> ::hyper::Response {
-            use hyper::header::{ContentLength, ContentType};
-            let msg = format!("{}", &self);
-            ::hyper::Response::new()
-                .with_status(::hyper::StatusCode::InternalServerError)
-                .with_header(ContentLength(msg.len() as u64))
-                .with_header(ContentType::plaintext())
-                .with_body(msg)
-        }
-    }
-
+fn not_found(_: ::reset_router::Context) -> Result<Response, Response> {
+    Ok(::http::Response::builder()
+        .status(200)
+        .body("Not found".into())
+        .unwrap())
 }
-
-mod utils {
-    pub fn mime_for<P: AsRef<::std::path::Path>>(path: P) -> ::hyper::mime::Mime {
-        lazy_static! {
-            pub static ref MIME_TYPES: ::conduit_mime_types::Types =
-                ::conduit_mime_types::Types::new().unwrap();
-        }
-
-        let path_as_ref = path.as_ref();
-
-        MIME_TYPES
-            .mime_for_path(path_as_ref)
-            .parse::<::hyper::mime::Mime>()
-            .expect("Unknown MIME type")
-    }
-}
-
-type BoxFuture<I, E> = Box<Future<Item = I, Error = E>>;
-type BoxedResponse = BoxFuture<hyper::Response, err::Error>;
-
-fn not_found(_: Context) -> BoxedResponse {
-    let msg = "NOT FOUND";
-    Box::new(::futures::future::ok(
-        Response::new()
-            .with_status(::hyper::StatusCode::NotFound)
-            .with_header(ContentLength(msg.len() as u64))
-            .with_header(ContentType::plaintext())
-            .with_body(msg),
-    ))
-}
-
-fn other(_: Context) -> err::Result<Response> {
-    let msg = "OTHER";
-    let response = Response::new()
-        .with_status(::hyper::StatusCode::Ok)
-        .with_header(ContentLength(msg.len() as u64))
-        .with_header(ContentType::plaintext())
-        .with_body(msg);
-    Ok(response)
-}
-
-fn post_body(ctx: Context) -> BoxedResponse {
-
-    let body = ctx.into_request().body();
-
-    Box::new(body.concat2().from_err().map(|bytes| {
-        Response::new()
-            .with_status(::hyper::StatusCode::Ok)
-            .with_header(ContentLength(bytes.len() as u64))
-            .with_header(ContentType::plaintext())
-            .with_body(bytes)
-    }))
-}
-
-fn assets(ctx: Context) -> BoxedResponse {
-
-    lazy_static! {
-        static ref FS_POOL: ::futures_fs::FsPool = ::futures_fs::FsPool::new(4);
-    }
-
-    // Don't do this with a public webserver
-    // You will potentially expose any file on your server to user requests
-
-    let path = {
-        let (p,): (String,) = ctx.extract_captures().unwrap();
-        ::std::path::Path::new(&::std::env::var("ASSETS_BASE").unwrap()).join(&p)
-    };
-
-    let mime = utils::mime_for(&path);
-
-    Box::new(FS_POOL.read(path).concat2().from_err().map(|bytes| {
-        Response::new()
-            .with_status(::hyper::StatusCode::Ok)
-            .with_header(ContentLength(bytes.len() as u64))
-            .with_header(ContentType(mime))
-            .with_body(bytes)
-    }))
-
-}
-
-
 
 fn main() {
-
-    use reset_router::hyper::ext::ServiceExtensions;
-
-    let addr = "0.0.0.0:3000".parse().unwrap();
-
-    let router = Router::build()
-        .add_get(r"\A/assets/(.+)\z", assets)
-        .add_get(r"\A/other\z", other)
-        .add_post(r"\A/post_body\z", post_body)
+    let router = ::reset_router::Router::build()
         .add_not_found(not_found)
         .finish()
         .unwrap();
 
-    router
-        .quick_serve(num_cpus::get(), addr, || {
-            ::tokio_core::reactor::Core::new().unwrap()
-        })
-        .unwrap();
+    let router = Arc::new(router);
+
+    let addr = "0.0.0.0:3000".parse().unwrap();
+    let server = ::hyper::server::Http::new().bind_compat(&addr, move || Ok(router.clone())).unwrap();
+    server.run().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,233 +1,112 @@
-//! A [`RegexSet`](https://doc.rust-lang.org/regex/regex/struct.RegexSet.html) based router for use with Rust web servers
-//! (currently only supports Hyper 0.11.6).
-//!
-//! Similar to and inspired by [reroute](https://github.com/gsquire/reroute), but  potentially
-//! faster (no unnecessary string allocations, no hashmaps, and method-first-matching).
-//!
-//! When used with Hyper, enables request handling for functions that look like `Fn(Request) -> RESPONSE`
-//! where `Request` is a thin wrapper around `hyper::server::Request` and
-//!
-//! ```rust,ignore
-//! RESPONSE: IntoFuture<Future = F, Item = S, Error = E>,
-//!     F: Future<Item = S, Error = E> + 'static + Send,
-//!     S: IntoResponse,
-//!     E: IntoResponse
-//! ```
-//!
-//! This means you can return something as simple as `Ok(Response::new())`. You don't have to worry about futures
-//! unless you need to read the request body or interact with other future-aware things.
-//!
-//! When used with Hyper, usage looks like:
-//!
-//! ```rust,ignore
-//! let router = Router::build()
-//!     .add(Method::Get, r"\A/\z", |_| Ok::<_, Response>(Response::new().with_body("ROOT path")))
-//!     .add_not_found(|_| Ok::<_, Response>(Response::new().with_body("Route not found")))
-//!     .finish()
-//!     .unwrap();
-//! router.quick_serve(8, "0.0.0.0:3000".parse().unwrap(), || Core::new().unwrap() );
-//! ```
-//!
-//! See [simple.rs](https://github.com/kardeiz/reset-router/blob/master/examples/simple.rs) for an example of use with Hyper.
-
 extern crate regex;
 extern crate http;
-
-// extern crate hyper;
+extern crate tokio_service;
+extern crate futures;
+extern crate hyper;
 
 #[macro_use]
-extern crate error_chain;
+extern crate failure;
 
-use http::Method;
+use self::futures::{Future, IntoFuture};
+
+pub mod err {
+    #[derive(Fail, Debug)]
+    pub enum Error {
+        #[fail(display = "Not found handler not set")]
+        NotFoundNotSet,
+        #[fail(display = "Could not parse captures")]
+        Captures,
+        #[fail(display = "Method not supported")]
+        MethodNotSupported,
+        #[fail(display = "{}", _0)]
+        Io(#[cause] ::std::io::Error),
+        #[fail(display = "{}", _0)]
+        Regex(#[cause] ::regex::Error)
+    }
+
+    pub type Result<T> = ::std::result::Result<T, Error>;
+}
 
 use regex::{Captures, Regex, RegexSet};
-
-use std::default::Default;
-use std::ops::{Deref, DerefMut};
+use http::{Method, Request, Response};
+use hyper::Body;
 use std::str::FromStr;
 use std::sync::Arc;
 
-pub mod err {
+type BoxFuture<I, E> = Box<Future<Item = I, Error = E>>;
 
-    //! Error handling with `error-chain`
-    error_chain! {
-        errors {
-            NotFound {
-                description("Path not found")
-            }
-            CapturesError {
-                description("Could not parse captures")
-            }
-            MethodNotSupported {
-                description("Method not supported")
-            }
-            Regex(t: String) {
-                description("regex error")
-                display("regex error: '{}'", t)
-            }
-        }
-
-        foreign_links {
-            Io(::std::io::Error);
-        }
-    }
-
-    impl From<::regex::Error> for Error {
-        fn from(t: ::regex::Error) -> Self {
-            Error::from_kind(ErrorKind::Regex(t.to_string()))
-        }
-    }
-
+pub struct Context {
+    request: Request<Body>,
+    regex_match: Option<Arc<Regex>>
 }
 
-#[cfg(feature = "hyper")]
-pub mod hyper;
-
-pub trait RequestLike {
-    fn path(&self) -> &str;
-    fn query(&self) -> Option<&str>;
-    fn method(&self) -> &Method;
-}
-
-pub struct Context<R> {
-    request: R,
-    regex_match: Option<Arc<Regex>>,
-}
-
-impl<R> Deref for Context<R> {
-    type Target = R;
-
-    fn deref(&self) -> &Self::Target {
-        &self.request
-    }
-}
-
-impl<R> DerefMut for Context<R> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.request
-    }
-}
-
-impl<R: RequestLike> Context<R> {
-    /// Captures (if any) from the matched path regex
+impl Context {
     pub fn captures(&self) -> Option<Captures> {
-        self.regex_match.as_ref().and_then(|r| {
-            r.captures(RequestLike::path(&self.request))
-        })
+        self.regex_match.as_ref()
+            .and_then(|r| r.captures(&self.request.uri().path()))
     }
 
-    /// Parsed capture segments
-    ///
-    /// Use like:
-    ///
-    /// ```rust,ignore
-    /// let (id, slug): (i32, String) = req.extract_captures().unwrap();
-    /// ```
-    pub fn extract_captures<C: CaptureExtracting<R>>(&self) -> Result<C, err::Error> {
-        Ok(C::extract_captures(self)?)
-    }
-
-    /// Consume and return the inner `RequestLike` object
-    pub fn into_request(self) -> R {
-        self.request
-    }
-
-    #[deprecated]
-    pub fn into_inner(self) -> R {
-        self.into_request()
-    }
-
-    pub fn from_request_and_regex(request: R, regex_match: Option<Arc<Regex>>) -> Self {
-        Context {
-            request,
-            regex_match,
-        }
+    pub fn parsed_captures<C: CaptureParsing>(&self) -> err::Result<C> {
+        Ok(C::parse_captures(self)?)
     }
 }
 
-pub trait CaptureExtracting<R>: Sized {
-    fn extract_captures(req: &Context<R>) -> err::Result<Self>;
+pub trait CaptureParsing: Sized {
+    fn parse_captures(ctx: &Context) -> err::Result<Self>;
 }
 
-impl<R, T> CaptureExtracting<R> for (T,)
-where
-    R: RequestLike,
-    T: FromStr,
-{
-    fn extract_captures(context: &Context<R>) -> err::Result<Self> {
-        let caps = context.captures().ok_or(err::ErrorKind::CapturesError)?;
-        let out_1 = caps.get(1)
+
+impl<U: FromStr> CaptureParsing for (U,) {
+    fn parse_captures(ctx: &Context) -> err::Result<Self> {
+        let captures = ctx.captures().ok_or(err::Error::Captures)?;
+        let out_1 = captures.get(1)
             .map(|x| x.as_str())
             .and_then(|x| x.parse().ok())
-            .ok_or(err::ErrorKind::CapturesError)?;
+            .ok_or(err::Error::Captures)?;
         Ok((out_1,))
     }
 }
 
-impl<R, T1, T2> CaptureExtracting<R> for (T1, T2)
-where
-    R: RequestLike,
-    T1: FromStr,
-    T2: FromStr,
-{
-    fn extract_captures(context: &Context<R>) -> err::Result<Self> {
-        let caps = context.captures().ok_or(err::ErrorKind::CapturesError)?;
-        let out_1 = caps.get(1)
+impl<U1: FromStr, U2: FromStr> CaptureParsing for (U1, U2) {
+    fn parse_captures(ctx: &Context) -> err::Result<Self> {
+        let captures = ctx.captures().ok_or(err::Error::Captures)?;
+        let out_1 = captures.get(1)
             .map(|x| x.as_str())
             .and_then(|x| x.parse().ok())
-            .ok_or(err::ErrorKind::CapturesError)?;
-        let out_2 = caps.get(2)
+            .ok_or(err::Error::Captures)?;
+        let out_2 = captures.get(2)
             .map(|x| x.as_str())
             .and_then(|x| x.parse().ok())
-            .ok_or(err::ErrorKind::CapturesError)?;
+            .ok_or(err::Error::Captures)?;
         Ok((out_1, out_2))
     }
 }
 
-impl<R, T1, T2, T3> CaptureExtracting<R> for (T1, T2, T3)
-where
-    R: RequestLike,
-    T1: FromStr,
-    T2: FromStr,
-    T3: FromStr,
-{
-    fn extract_captures(context: &Context<R>) -> err::Result<Self> {
-        let caps = context.captures().ok_or(err::ErrorKind::CapturesError)?;
-        let out_1 = caps.get(1)
+impl<U1: FromStr, U2: FromStr, U3: FromStr> CaptureParsing for (U1, U2, U3) {
+    fn parse_captures(ctx: &Context) -> err::Result<Self> {
+        let captures = ctx.captures().ok_or(err::Error::Captures)?;
+        let out_1 = captures.get(1)
             .map(|x| x.as_str())
             .and_then(|x| x.parse().ok())
-            .ok_or(err::ErrorKind::CapturesError)?;
-        let out_2 = caps.get(2)
+            .ok_or(err::Error::Captures)?;
+        let out_2 = captures.get(2)
             .map(|x| x.as_str())
             .and_then(|x| x.parse().ok())
-            .ok_or(err::ErrorKind::CapturesError)?;
-        let out_3 = caps.get(3)
+            .ok_or(err::Error::Captures)?;
+        let out_3 = captures.get(3)
             .map(|x| x.as_str())
             .and_then(|x| x.parse().ok())
-            .ok_or(err::ErrorKind::CapturesError)?;
+            .ok_or(err::Error::Captures)?;
         Ok((out_1, out_2, out_3))
     }
 }
 
-/// Provides a way to convert some item into a boxed handler.
-///
-/// This is useful to converge e.g. disparate functions into a single signature.
-
-pub trait IntoBoxedHandler<T, S> {
-    fn into_boxed_handler(self) -> Box<Handler<T, S>>
-    where
-        Self: Sized + 'static;
+pub trait Handler: Send + Sync {
+    fn handle(&self, Context) -> BoxFuture<Response<Body>, ::hyper::Error>;
 }
 
-impl<T, S> IntoBoxedHandler<T, S> for Box<Handler<T, S>> {
-    fn into_boxed_handler(self) -> Box<Handler<T, S>> {
-        self
-    }
-}
-
-/// The handler. For example, takes a `Request` and returns a `Response`.
-pub trait Handler<T, S>: Send + Sync {
-    fn handle(&self, T) -> S;
+pub trait IntoBoxedHandler {
+    fn into_boxed_handler(self) -> Box<Handler> where Self: Sized + 'static;
 }
 
 #[derive(Default)]
@@ -249,7 +128,7 @@ impl<T> MethodMap<T> {
             Method::PATCH => Ok(&self.patch),
             Method::HEAD => Ok(&self.head),
             Method::DELETE => Ok(&self.delete),
-            _ => Err(err::ErrorKind::MethodNotSupported.into()),
+            _ => Err(err::Error::MethodNotSupported),
         }
     }
 
@@ -261,43 +140,39 @@ impl<T> MethodMap<T> {
             Method::PATCH => Ok(&mut self.patch),
             Method::HEAD => Ok(&mut self.head),
             Method::DELETE => Ok(&mut self.delete),
-            _ => Err(err::ErrorKind::MethodNotSupported.into()),
+            _ => Err(err::Error::MethodNotSupported),
         }
     }
 }
 
-pub struct PathHandlers<T, S> {
+pub struct PathHandlers {
     regex_set: RegexSet,
     regexes: Vec<Arc<Regex>>,
     priorities: Vec<usize>,
-    handlers: Vec<Box<Handler<T, S>>>,
+    handlers: Vec<Box<Handler>>,
 }
 
-/// The "finished" `Router`. See [`RouterBuilder`](/reset-router/*/reset_router/struct.RouterBuilder.html) for building a `Router`.
-pub struct Router<T, S> {
-    not_found: Option<Box<Handler<T, S>>>,
-    handlers: MethodMap<Option<PathHandlers<T, S>>>,
+pub struct Router {
+    not_found: Box<Handler>,
+    handlers: MethodMap<Option<PathHandlers>>,
 }
 
-impl<T, S> Router<T, S> {
-    pub fn build<'a>() -> RouterBuilder<'a, T, S> {
+impl Router {
+    pub fn build<'a>() -> RouterBuilder<'a> {
         RouterBuilder::new()
     }
 
-    fn find_handler_and_context<R: RequestLike>(
-        &self,
-        request: R,
-    ) -> err::Result<(&Box<Handler<T, S>>, Context<R>)> {
+    fn find_handler_and_context(&self, request: Request<Body>) -> (&Box<Handler>, Context) {
         if let Some(path_handlers) =
             self.handlers
-                .get(&RequestLike::method(&request))
+                .get(request.method())
                 .ok()
                 .and_then(|x| x.as_ref())
         {
             let priorities = &path_handlers.priorities;
             if let Some(i) = path_handlers
                 .regex_set
-                .matches(RequestLike::path(&request))
+                .matches(request.uri().path())
                 .iter()
                 .min_by(|x, y| priorities[*x].cmp(&priorities[*y]))
             {
@@ -305,32 +180,23 @@ impl<T, S> Router<T, S> {
                 let handler = &path_handlers.handlers[i];
                 let regex = &path_handlers.regexes[i];
 
-                let context = Context::from_request_and_regex(request, Some(regex.clone()));
+                let context = Context { request, regex_match: Some(regex.clone()) };
 
-                return Ok((handler, context));
+                return (handler, context);
             }
         }
 
-        if let Some(handler) = self.not_found.as_ref() {
-            return Ok((handler, Context::from_request_and_regex(request, None)));
-        }
-
-        Err(err::ErrorKind::NotFound.into())
+        (&self.not_found, Context { request, regex_match: None })
 
     }
 }
 
-/// Builder for a [`Router`](/reset-router/*/reset_router/struct.Router.html)
-///
-/// Please note that you can assign a priority to a handler with `add_with_priority`.
-///
-/// Default priority is 0. Lowest priority (closer to 0) wins.
-pub struct RouterBuilder<'a, T, S> {
-    not_found: Option<Box<Handler<T, S>>>,
-    path_handler_parts: Vec<(Method, &'a str, usize, Box<Handler<T, S>>)>,
+pub struct RouterBuilder<'a> {
+    not_found: Option<Box<Handler>>,
+    path_handler_parts: Vec<(Method, &'a str, usize, Box<Handler>)>,
 }
 
-impl<'a, T, S> RouterBuilder<'a, T, S> {
+impl<'a> RouterBuilder<'a> {
     pub fn new() -> Self {
         RouterBuilder {
             not_found: None,
@@ -340,7 +206,7 @@ impl<'a, T, S> RouterBuilder<'a, T, S> {
 
     pub fn add_not_found<H>(mut self, handler: H) -> Self
     where
-        H: IntoBoxedHandler<T, S> + 'static,
+        H: IntoBoxedHandler + 'static,
     {
         self.not_found = Some(handler.into_boxed_handler());
         self
@@ -348,7 +214,7 @@ impl<'a, T, S> RouterBuilder<'a, T, S> {
 
     pub fn add<H>(mut self, method: Method, regex: &'a str, handler: H) -> Self
     where
-        H: IntoBoxedHandler<T, S> + 'static,
+        H: IntoBoxedHandler + 'static,
     {
         self.path_handler_parts.push((
             method,
@@ -367,7 +233,7 @@ impl<'a, T, S> RouterBuilder<'a, T, S> {
         handler: H,
     ) -> Self
     where
-        H: IntoBoxedHandler<T, S> + 'static,
+        H: IntoBoxedHandler + 'static,
     {
         self.path_handler_parts.push((
             method,
@@ -378,7 +244,7 @@ impl<'a, T, S> RouterBuilder<'a, T, S> {
         self
     }
 
-    pub fn finish(self) -> err::Result<Router<T, S>> {
+    pub fn finish(self) -> err::Result<Router> {
 
         let mut path_handlers_map = ::std::collections::HashMap::new();
 
@@ -393,15 +259,15 @@ impl<'a, T, S> RouterBuilder<'a, T, S> {
         }
 
         let mut router = Router {
-            not_found: self.not_found,
+            not_found: self.not_found.ok_or_else(|| err::Error::NotFoundNotSet)?,
             handlers: MethodMap::default(),
         };
 
         for (method, (paths, priorities, handlers)) in path_handlers_map {
-            let regex_set = RegexSet::new(paths.iter())?;
+            let regex_set = RegexSet::new(paths.iter()).map_err(err::Error::Regex)?;
             let mut regexes = Vec::new();
             for path in &paths {
-                regexes.push(Arc::new(Regex::new(path)?));
+                regexes.push(Arc::new(Regex::new(path).map_err(err::Error::Regex)?));
             }
 
             let path_handlers = PathHandlers {
@@ -418,40 +284,45 @@ impl<'a, T, S> RouterBuilder<'a, T, S> {
     }
 }
 
-macro_rules! __build__ {
-    ($([$add_method:ident, $add_method_with_priority:ident, $method:path]),+) => {
-
-        $(
-            pub fn $add_method<H>(self, regex: &'a str, handler: H) -> Self
-            where
-                H: IntoBoxedHandler<T, S> + 'static
-            {
-                self.add($method, regex, handler)
-            }
-
-            pub fn $add_method_with_priority<H>(
-                self,
-                regex: &'a str,
-                priority: usize,
-                handler: H
-            ) -> Self
-            where
-                H: IntoBoxedHandler<T, S> + 'static
-            {
-                self.add_with_priority($method, regex, priority, handler)
-            }
-        )*
-
+impl<F> Handler for F
+where
+    F: Fn(Context) -> BoxFuture<Response<Body>, ::hyper::Error> + Send + Sync {
+    fn handle(&self, context: Context) -> BoxFuture<Response<Body>, ::hyper::Error> {
+        (self)(context)
     }
 }
 
-impl<'a, T, S> RouterBuilder<'a, T, S> {
-    __build__! {
-        [add_get, add_get_with_priority, Method::GET],
-        [add_post, add_post_with_priority, Method::POST],
-        [add_put, add_put_with_priority, Method::PUT],
-        [add_patch, add_patch_with_priority, Method::PATCH],
-        [add_head, add_head_with_priority, Method::HEAD],
-        [add_delete, add_delete_with_priority, Method::DELETE]
+impl<I, S, E, H> IntoBoxedHandler for H
+where
+    I: IntoFuture<Item = S, Error = E>,
+    I::Future: 'static,
+    S: Into<Response<Body>>,
+    E: Into<Response<Body>>,
+    H: Fn(Context) -> I + Sync + Send,
+{
+    fn into_boxed_handler(self) -> Box<Handler>
+    where
+        Self: Sized + 'static,
+    {
+        Box::new(move |context: Context| -> BoxFuture<Response<Body>, ::hyper::Error> {
+            Box::new(
+                (self)(context)
+                    .into_future()
+                    .map(|s| s.into())
+                    .or_else(|e| Ok(e.into())),
+            )
+        })
+    }
+}
+
+impl ::tokio_service::Service for Router {
+    type Request = Request<Body>;
+    type Response = Response<Body>;
+    type Error = ::hyper::Error;
+    type Future = BoxFuture<Response<Body>, ::hyper::Error>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        let (handler, ctx) = self.find_handler_and_context(req);
+        handler.handle(ctx)
     }
 }
